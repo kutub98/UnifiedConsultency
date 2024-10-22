b4b5d22e7cd94cedb75bfaeb64a86d359f9c07ef
Kutub Udd <socialnetwork98710@gmail.com>
2024-10-22 20:04:16 +0600

    added scrollAnimate

--- src/app/Components/Shared/ScrollProgressBar.js
+++ src/app/Components/Shared/ScrollProgressBar.js
@@ -1,12 +1,17 @@
 "use client";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 import React from "react";
 const ScrollProgressBar = () => {
   const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
   return (
     <motion.div
       className="   w-full max-w-7xl mx-auto z-[999]  h-1 bg-red-900"
-      style={{ scaleX: scrollYProgress }}
+      style={{ scaleX }}
     ></motion.div>
   );
 };
